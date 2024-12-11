0133b8d6d0667ba917eaa8ac9805d0b5e1d3eb86
Sidhart Krishnan <sidhartkrishnan@gmail.com>
2024-12-11 02:45:21 -0800

    Updated it to use the large df for statistics

--- data-processing.py
+++ data-processing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv("./SMT_2024/SMT_Algebra_2024_Small.csv").rename(columns={"#": "id"})
+big_df = pd.read_csv("./SMT_2024/SMT_Algebra_2024.csv").rename(columns={"#": "id"})
 
 # sort by id
 df.sort_values(by=['id'], inplace=True)
@@ -14,13 +15,13 @@ for i in range(1, 11):
     # Create a freq_col
     df[freq_col] = 0.0
     
-    counts = df[df[correct_col] == 0][col].value_counts()
+    counts = big_df[big_df[correct_col] == 0][col].value_counts()
     df.loc[df[correct_col] == 0, freq_col] = df.loc[df[correct_col] == 0, col].map(counts/counts.sum())
     df.loc[df[correct_col] == 1, freq_col] = 0.0
 
 for i in range(1, 11):
     correct_perc = f"C{i}_perc"
-    df[correct_perc] = df[f"C{i}"].mean()
+    df[correct_perc] = big_df[f"C{i}"].mean()
 
 
 # Add the Start Time to the times in 'T1' to 'T10'
